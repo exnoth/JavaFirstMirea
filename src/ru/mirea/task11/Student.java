774eb1f41197fb5b15d5f8112ac3bc48d5cce382
exnoth <lilfreakinggurl@gmail.com>
2022-10-25 14:53:29 +0300

    fcomm

---
+++ src/ru/mirea/task11/Student.java
@@ -0,0 +1,70 @@
+package ru.mirea.task11;
+
+public class Student
+{
+    String name;
+    String surname;
+    String specialty;
+    int year;
+    String group;
+
+    public Student(String name, String surname, String specialty, int year, String group)
+    {
+        setName(name);
+        setSurname(surname);
+        setSpecialty(specialty);
+        setYear(year);
+        setGroup(group);
+    }
+
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public void setSpecialty(String specialty)
+    {
+        this.specialty = specialty;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public void setGroup(String group)
+    {
+        this.group = group;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getSurname()
+    {
+        return this.surname;
+    }
+
+    public String getSpecialty()
+    {
+        return this.specialty;
+    }
+
+    public int getYear()
+    {
+        return this.year;
+    }
+
+    public String getGroup()
+    {
+        return this.group;
+    }
+}
