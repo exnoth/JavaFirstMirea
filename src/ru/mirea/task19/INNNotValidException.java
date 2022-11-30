8f583ce7b503018468fbe32831d4f81048300be6
exnoth <lilfreakinggurl@gmail.com>
2022-11-30 20:38:50 +0300

    fcomm

---
+++ src/ru/mirea/task19/INNNotValidException.java
@@ -0,0 +1,19 @@
+package ru.mirea.task19;
+
+import java.math.BigInteger;
+
+public class INNNotValidException extends Exception
+{
+    private BigInteger INNcode;
+
+    public INNNotValidException(BigInteger inn)
+    {
+        super("INN code " + inn + " is invalid!");
+        INNcode = inn;
+    }
+
+    public BigInteger getINNcode()
+    {
+        return INNcode;
+    }
+}
