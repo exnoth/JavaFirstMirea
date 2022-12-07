f7f1cd1605c41696bc579b49e2c9ce8b8f20f1d2
exnoth <lilfreakinggurl@gmail.com>
2022-12-08 01:08:33 +0300

    task 30

---
+++ src/ru/mirea/task30/OrderAlreadyException.java
@@ -0,0 +1,8 @@
+package ru.mirea.task30;
+
+public class OrderAlreadyException extends Exception
+{
+    public OrderAlreadyException(int num){
+        super("The table " + num + " is added!");
+    }
+}
